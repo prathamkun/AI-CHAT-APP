c3c6cf6c8c48bdfd1c7c0d72b39cc04b7f33ead4
prathamkun <216220075+prathamkun@users.noreply.github.com>
2026-03-09 21:42:07 +0530

    server update

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const mongoose = require("mongoose");
 require("dotenv").config();
 
 const chatRoute = require("./routes/chat");
@@ -8,6 +9,12 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+mongoose.connect("mongodb://127.0.0.1:27017/aichat");
+
+mongoose.connection.once("open", () => {
+  console.log("MongoDB connected");
+});
 
 app.use("/api/chat", chatRoute);
 
